b247f60900fc4596659f39f45fd21d795bb9e2d5
Mickey202505 <mickeyfarren@gmail.com>
2026-07-31 22:49:37 +0100

    feat(core): implement first working GreenDetector

--- packages/core/src/detectors/Green/GreenDetector.ts
+++ packages/core/src/detectors/Green/GreenDetector.ts
@@ -1,1 +1,25 @@
-throw new Error("Not implemented");
+import { BoundaryTracer } from "../../geometry/BoundaryTracer";
+import { PolygonOptimizer } from "../../geometry/PolygonOptimizer";
+import { ImageCropper } from "../../image/ImageCropper";
+import { ImageData } from "../../image/ImageData";
+
+export class GreenDetector {
+  private readonly cropper = new ImageCropper();
+  private readonly tracer = new BoundaryTracer();
+  private readonly optimizer = new PolygonOptimizer();
+
+  detect(image: ImageData<number>) {
+    const cropped = this.cropper.crop(image, {
+      x: 0,
+      y: 0,
+      width: image.width,
+      height: image.height,
+    });
+
+    const boundary = this.tracer.trace(cropped.pixels);
+
+    const polygon = this.optimizer.optimize(boundary);
+
+    return [polygon];
+  }
+}

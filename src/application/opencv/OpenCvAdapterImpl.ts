7f83d9f85d3998243f65483922e689de59d66cf6
Mickey202505 <mickeyfarren@gmail.com>
2026-08-12 22:07:35 +0100

    updates

--- src/application/opencv/OpenCvAdapterImpl.ts
+++ src/application/opencv/OpenCvAdapterImpl.ts
@@ -1,58 +1,71 @@
 import type {
-OpenCvContour,
-OpenCvMat,
-OpenCvRuntime,
-OpenCvPoint
+    OpenCvContour,
+    OpenCvMat,
+    OpenCvRuntime,
+    OpenCvPoint
 } from "./OpenCvTypes";
 import type { OpenCvAdapter } from "./OpenCvAdapter";
 
 export class OpenCvAdapterImpl implements OpenCvAdapter {
-private readonly cv: OpenCvRuntime;
+    private readonly cv: OpenCvRuntime;
 
-public constructor(cv: OpenCvRuntime) {
-    this.cv = cv;
-}
+    public constructor(cv: OpenCvRuntime) {
+        this.cv = cv;
+    }
 
-public findContours(
-    image: OpenCvMat
-): readonly OpenCvContour[] {
-    const contours = new this.cv.MatVector();
-    const hierarchy = new this.cv.Mat();
-
-    try {
-        this.cv.findContours(
-            image,
-            contours,
-            hierarchy,
-            this.cv.RETR_EXTERNAL,
-            this.cv.CHAIN_APPROX_SIMPLE
-        );
-
+    public findContours(
+        image: OpenCvMat
+    ): readonly OpenCvContour[] {
+        const contours = new this.cv.MatVector();
+        const hierarchy = new this.cv.Mat();
         const result: OpenCvContour[] = [];
 
-        for (let i = 0; i < contours.size(); i += 1) {
-            const contour = contours.get(i);
+        try {
+            this.cv.findContours(
+                image,
+                contours,
+                hierarchy,
+                this.cv.RETR_EXTERNAL,
+                this.cv.CHAIN_APPROX_SIMPLE
+            );
 
-            result.push({
-                points: this.readPoints(contour)
-            });
+            for (let i = 0; i < contours.size(); i += 1) {
+                const contour = contours.get(i);
 
-            contour.delete();
+                result.push({
+                    points: this.readPoints(contour)
+                });
+
+                contour.delete();
+            }
+
+            return result;
+        } finally {
+            hierarchy.delete();
+            contours.delete();
+        }
+    }
+
+    private readPoints(
+        contour: OpenCvMat
+    ): readonly OpenCvPoint[] {
+        const points: OpenCvPoint[] = [];
+
+        const data = (contour as OpenCvMat & {
+            data32S?: Int32Array;
+        }).data32S;
+
+        if (!data) {
+            return points;
         }
 
-        return result;
-    } finally {
-        hierarchy.delete();
-        contours.delete();
+        for (let i = 0; i + 1 < data.length; i += 2) {
+            points.push({
+                x: data[i] ?? 0,
+                y: data[i + 1] ?? 0
+            });
+        }
+
+        return points;
     }
-}
-
-private readPoints(
-    contour: OpenCvMat
-): readonly OpenCvPoint[] {
-    void contour;
-
-    return [];
-}
-
 }

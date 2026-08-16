c9856bcebb92f2b3a3c0e295a0582114831c78e0
Mickey202505 <mickeyfarren@gmail.com>
2026-08-16 21:07:09 +0100

    updates

--- src/infrastructure/opencv/OpenCvJsRuntime.ts
+++ src/infrastructure/opencv/OpenCvJsRuntime.ts
@@ -1,11 +1,22 @@
-import cv from "opencv.js";
 import type { OpenCvRuntime } from "../../application/opencv/OpenCvTypes";
 
-export const openCvRuntime: OpenCvRuntime = {
-    Mat: cv.Mat,
-    MatVector: cv.MatVector,
-    findContours: cv.findContours,
-    RETR_EXTERNAL: cv.RETR_EXTERNAL,
-    CHAIN_APPROX_SIMPLE: cv.CHAIN_APPROX_SIMPLE,
-    matFromImageData: cv.matFromImageData
-};
+declare global {
+    interface Window {
+        cv?: OpenCvRuntime;
+    }
+}
+
+function getOpenCvRuntime(): OpenCvRuntime {
+    const cv = window.cv;
+
+    if (cv === undefined) {
+        throw new Error(
+            "OpenCV.js has not finished loading."
+        );
+    }
+
+    return cv;
+}
+
+export const openCvRuntime: OpenCvRuntime =
+    getOpenCvRuntime();

a25de8b0b662fe792c543eeff9cf1d73ca2acdd3
Mickey202505 <mickeyfarren@gmail.com>
2026-08-17 15:56:45 +0100

    updates

--- src/application/opencv/OpenCvTypes.ts
+++ src/application/opencv/OpenCvTypes.ts
@@ -1,56 +1,56 @@
 export interface OpenCvMat {
-    readonly rows: number;
-    readonly cols: number;
-    readonly data32S?: Int32Array;
-    delete(): void;
+readonly rows: number;
+readonly cols: number;
+readonly data32S?: Int32Array;
+delete(): void;
 }
 
 export interface OpenCvPoint {
-    readonly x: number;
-    readonly y: number;
+readonly x: number;
+readonly y: number;
 }
 
 export interface OpenCvContour {
-    readonly points: readonly OpenCvPoint[];
+readonly points: readonly OpenCvPoint[];
 }
 
 export interface OpenCvContourCollection {
-    readonly size: () => number;
-    readonly get: (index: number) => OpenCvMat;
-    delete(): void;
+readonly size: () => number;
+readonly get: (index: number) => OpenCvMat;
+delete(): void;
 }
 
 export interface OpenCvImageData {
-    readonly width: number;
-    readonly height: number;
-    readonly data: Uint8ClampedArray;
+readonly width: number;
+readonly height: number;
+readonly data: Uint8ClampedArray;
 }
 
 export interface OpenCvRuntime {
-    readonly Mat: new () => OpenCvMat;
+readonly Mat: new () => OpenCvMat;
+readonly MatVector: new () => OpenCvContourCollection;
 
-    readonly MatVector: new () => OpenCvContourCollection;
+findContours(
+    image: OpenCvMat,
+    contours: OpenCvContourCollection,
+    hierarchy: OpenCvMat,
+    mode: number,
+    method: number
+): void;
 
-    findContours(
-        image: OpenCvMat,
-        contours: OpenCvContourCollection,
-        hierarchy: OpenCvMat,
-        mode: number,
-        method: number
-    ): void;
+readonly RETR_EXTERNAL: number;
+readonly CHAIN_APPROX_SIMPLE: number;
 
-    readonly RETR_EXTERNAL: number;
-    readonly CHAIN_APPROX_SIMPLE: number;
+readonly matFromImageData?: (
+    imageData: OpenCvImageData
+) => OpenCvMat;
 
-    readonly matFromImageData?: (
-        imageData: OpenCvImageData
-    ) => OpenCvMat;
+readonly cvtColor?: (
+    src: OpenCvMat,
+    dst: OpenCvMat,
+    code: number
+) => void;
 
-    readonly cvtColor?: (
-        source: OpenCvMat,
-        destination: OpenCvMat,
-        code: number
-    ) => void;
+readonly COLOR_RGBA2GRAY?: number;
 
-    readonly COLOR_RGBA2GRAY?: number;
 }

446a195176c51d34ea21800638524bbef6adf07e
Mickey202505 <mickeyfarren@gmail.com>
2026-08-24 17:56:28 +0100

    update

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,14 @@
 console.log("Loading OpenCV.js...");
+
 const cv = require("opencv.js");
-console.log("OpenCV.js loaded:", Object.keys(cv).length, "keys");
-global.window = global as any;
+
+console.log(
+    "OpenCV.js loaded:",
+    Object.keys(cv).length,
+    "keys"
+);
+
+global.window = globalThis as typeof global.window;
 global.window.cv = cv;
+
 console.log("OpenCV.js attached to window.");
